8a82f5e1d496051f2873cdd106e5a7dcdccf2b44
kuba-nychbek <koba.orozbekov.99@gmail.com>
2024-04-03 21:13:48 +0600

    First attempt

--- core/settings.py
+++ core/settings.py
@@ -26,8 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
-    'api'
-    'rest_framework'
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
